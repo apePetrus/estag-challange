548629e0a95f03f35e00730985c505f2dd99cea5
root <ppereira@softexpert.com>
2024-11-04 10:48:40 -0300

    STYLE: removed obsolete/unused/useless scripts and folders

--- front/src/App.js
+++ front/src/App.js
@@ -20,43 +20,48 @@ import Home from './pages/Home.js';
 import StoreCategories from './pages/StoreCategories';
 import Pdv from './pages/Pdv';
 
-// const RouterContext = createContext();
 
-const routerCustomer = createBrowserRouter([
-  {
-    path: '/',
-    element: <LayoutStore />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: '/categories', element: <StoreCategories /> },
-      // { path: '/', element: <Pdv /> }, 
-    ],
-  },
-]);
 //
 //
 // END OF FUTURE STUFF TO WORK WITH
 
 
 
-const routerEmployee = createBrowserRouter([
-  {
-    path: '/',
-    element: <LayoutPdv />,
-    children: [
-      { path: '/', element: <PdvCart /> },
-      { path: '/products', element: <Products /> },
-      { path: '/categories', element: <PdvCategories /> },
-      { path: '/history', element: <History /> },
-    ]
-  }
-]);
 
 function App() {
-  // const [ routerLayout, setRouterLayout ] = useContext(RouterContext);
+
+  const [ routerLayout, setRouterLayout ] = useState();
+
+  const routerEmployee = createBrowserRouter([
+    {
+      path: '/',
+      element: <LayoutPdv routerLayout={routerLayout}/>,
+      children: [
+        { path: '/', element: <PdvCart /> },
+        { path: '/products', element: <Products /> },
+        { path: '/categories', element: <PdvCategories /> },
+        { path: '/history', element: <History /> },
+      ]
+    }
+  ]);
+
+
+  const routerCustomer = createBrowserRouter([
+    {
+      path: '/',
+      element: <LayoutStore />,
+      children: [
+        { path: '/', element: <PdvCart /> },
+        { path: '/categories', element: <StoreCategories /> },
+        // { path: '/', element: <Pdv /> }, 
+      ],
+    },
+  ]);
+
+  // setRouterLayout({routerEmployee})
 
   return (
-      <RouterProvider router={routerEmployee} />
+      <RouterProvider router={routerCustomer} />
 
   );
 }
